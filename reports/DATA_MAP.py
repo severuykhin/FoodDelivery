0b6f4c93099e3542c2370af4aa1a99044cfe782d
Igor <severuykhin.igor@gmail.com>
2019-10-08 09:17:36 +0300

    consolidated report

---
+++ reports/DATA_MAP.py
@@ -0,0 +1,115 @@
+# Describes the map of data-structure
+def createMap():
+    return {
+        "query_groups": [
+            {
+                "name": "еда",
+                "group_negative_keys": ["обед"],
+                "search_keys": ["ед"],
+                "queries_data": [],
+                "impressions": 0,
+                "clicks": 0,
+                "conversions": 0
+            },
+            {
+                "name": "Пельмени",
+                "group_negative_keys": [],
+                "search_keys": ["пельмен"],
+                "queries_data": [],
+                "impressions": 0,
+                "clicks": 0,
+                "conversions": 0
+            },
+            {
+                "name": "Пицца",
+                "group_negative_keys": [],
+                "search_keys": ["пицц"],
+                "queries_data": [],
+                "impressions": 0,
+                "clicks": 0,
+                "conversions": 0
+            },
+            {
+                "name": "Роллы",
+                "group_negative_keys": [],
+                "search_keys": ["ролл"],
+                "queries_data": [],
+                "impressions": 0,
+                "clicks": 0,
+                "conversions": 0
+            },
+            {
+                "name": "Обеды",
+                "group_negative_keys": [],
+                "search_keys": ["обед"],
+                "queries_data": [],
+                "impressions": 0,
+                "clicks": 0,
+                "conversions": 0
+            },
+            {
+                "name": "Ужин",
+                "group_negative_keys": [],
+                "search_keys": ["ужин"],
+                "queries_data": [],
+                "impressions": 0,
+                "clicks": 0,
+                "conversions": 0
+            },
+            {
+                "name": "Чебурек",
+                "group_negative_keys": [],
+                "search_keys": ["чебур"],
+                "queries_data": [],
+                "impressions": 0,
+                "clicks": 0,
+                "conversions": 0
+            },
+            {
+                "name": "Суп",
+                "group_negative_keys": [],
+                "search_keys": ["суп"],
+                "queries_data": [],
+                "impressions": 0,
+                "clicks": 0,
+                "conversions": 0
+            },
+            {
+                "name": "Салат",
+                "group_negative_keys": [],
+                "search_keys": ["салат"],
+                "queries_data": [],
+                "impressions": 0,
+                "clicks": 0,
+                "conversions": 0
+            },
+            {
+                "name": "Удон",
+                "group_negative_keys": [],
+                "search_keys": ["удон"],
+                "queries_data": [],
+                "impressions": 0,
+                "clicks": 0,
+                "conversions": 0
+            },
+            {
+                "name": "Домашняя еда",
+                "group_negative_keys": [],
+                "search_keys": ["домашн"],
+                "queries_data": [],
+                "impressions": 0,
+                "clicks": 0,
+                "conversions": 0
+            },
+            {
+                "name": "Лапша",
+                "group_negative_keys": [],
+                "search_keys": ["лапш", "удон", "вок"],
+                "queries_data": [],
+                "impressions": 0,
+                "clicks": 0,
+                "conversions": 0
+            },
+        ],
+        "negative_words": ["йола"]
+    }
--- reports/queries.py
+++ reports/queries.py
@@ -1,110 +1,25 @@
 import csv
+from DATA_MAP import createMap
+
+MAP = createMap()
 
 # Define variables
 SOURCE_CSV = "webmaster-summary.csv"
 TARGET_CSV = "queries-reports.csv"
+CONVERSIONS_CSV = "conversions.csv"
 
-MAP = {
-    "query_groups": [
-        {
-            "name": "еда",
-            "group_negative_keys": ["обед"],
-            "search_keys": ["ед"],
-            "queries_data": [],
-            "impressions": 0,
-            "clicks": 0
-        },
-        {
-            "name": "Пельмени",
-            "group_negative_keys": [],
-            "search_keys": ["пельмен"],
-            "queries_data": [],
-            "impressions": 0,
-            "clicks": 0
-        },
-        {
-            "name": "Пицца",
-            "group_negative_keys": [],
-            "search_keys": ["пицц"],
-            "queries_data": [],
-            "impressions": 0,
-            "clicks": 0
-        },
-        {
-            "name": "Роллы",
-            "group_negative_keys": [],
-            "search_keys": ["ролл"],
-            "queries_data": [],
-            "impressions": 0,
-            "clicks": 0
-        },
-        {
-            "name": "Обеды",
-            "group_negative_keys": [],
-            "search_keys": ["обед"],
-            "queries_data": [],
-            "impressions": 0,
-            "clicks": 0
-        },
-        {
-            "name": "Ужин",
-            "group_negative_keys": [],
-            "search_keys": ["ужин"],
-            "queries_data": [],
-            "impressions": 0,
-            "clicks": 0
-        },
-        {
-            "name": "Чебурек",
-            "group_negative_keys": [],
-            "search_keys": ["чебур"],
-            "queries_data": [],
-            "impressions": 0,
-            "clicks": 0
-        },
-        {
-            "name": "Суп",
-            "group_negative_keys": [],
-            "search_keys": ["суп"],
-            "queries_data": [],
-            "impressions": 0,
-            "clicks": 0
-        },
-        {
-            "name": "Салат",
-            "group_negative_keys": [],
-            "search_keys": ["салат"],
-            "queries_data": [],
-            "impressions": 0,
-            "clicks": 0
-        },
-        {
-            "name": "Удон",
-            "group_negative_keys": [],
-            "search_keys": ["удон"],
-            "queries_data": [],
-            "impressions": 0,
-            "clicks": 0
-        },
-        {
-            "name": "Домашняя еда",
-            "group_negative_keys": [],
-            "search_keys": ["домашн", "ед"],
-            "queries_data": [],
-            "impressions": 0,
-            "clicks": 0
-        },
-        {
-            "name": "Лапша",
-            "group_negative_keys": [],
-            "search_keys": ["лапш", "удон", "вок"],
-            "queries_data": [],
-            "impressions": 0,
-            "clicks": 0
-        },
-    ],
-    "negative_words": ["йола"]
-}
+CONVERSIONS = []
+
+
+def csv_to_array(file_obj):
+    reader = csv.reader(file_obj)
+    for idx, row in enumerate(reader):
+        if (idx == 0 or idx == 1): continue
+        data = { 
+            "query": row[0], 
+            "count": row[2] 
+        }
+        CONVERSIONS.append(data)
 
 def write_row(data):
     with open(TARGET_CSV, "a") as report_file_obj:
@@ -113,7 +28,8 @@ def write_row(data):
             data["phrase"], 
             data["impressions"], 
             data["clicks"], 
-            data["ctr"]
+            data["ctr"],
+            data["conversions"]
         ])
 
 def read_queries(file_obj):
@@ -146,14 +62,29 @@ def read_queries(file_obj):
                             if neg_key in query:
                                 mismatch = True
 
-                    if mismatch == False:        
-                        group["queries_data"].append(row)
-                        group["impressions"] += int(float(row[2]))
-                        group["clicks"] += int(float(row[3]))
+                    if mismatch == True: continue
+
+                    group["impressions"] += int(float(row[2]))
+                    group["clicks"] += int(float(row[3]))
+
+                    conversionsArr = list(filter(lambda  item: (item["query"] == query), CONVERSIONS))
+
+                    queryConversionsCount = 0
+
+                    # Add conversions data to impressions row
+                    for item in conversionsArr:
+                        queryConversionsCount = queryConversionsCount + int(item["count"])
+                        group['conversions'] = group['conversions'] + int(item["count"])
+
+                    row.append(queryConversionsCount)
+                    group["queries_data"].append(row)
 
 def process_queries():
+
+    MAP['query_groups'].sort(key=lambda group: group['impressions'], reverse=True)
+
     for group in MAP['query_groups']:
-        group["queries_data"].sort(key = lambda x: x[1])
+        group["queries_data"].sort(key=lambda x: x[1])
 
 def build_report():
 
@@ -161,7 +92,8 @@ def build_report():
         "phrase": "Группа",
         "impressions": "Показы",
         "clicks": "Клики",
-        "ctr": "CTR"
+        "ctr": "CTR",
+        "conversions": "Конверсии"
     })
 
     for group in MAP['query_groups']:
@@ -170,7 +102,8 @@ def build_report():
             "phrase": group["name"],
             "impressions": group["impressions"],
             "clicks": group["clicks"],
-            "ctr": ''
+            "ctr": '',
+            "conversions": group["conversions"]
         }
 
         write_row(group_summary_info)
@@ -181,6 +114,7 @@ def build_report():
                 "impressions": row[2],
                 "clicks": row[3],
                 "ctr": row[4],
+                "conversions": row[25]
             }
             write_row(data)
 
@@ -191,6 +125,9 @@ if __name__ == "__main__":
     report = open(TARGET_CSV, "w")
     report.truncate()
     report.close()
+
+    with open(CONVERSIONS_CSV) as conversions_file_obj:
+        csv_to_array(conversions_file_obj)
 
     with open(SOURCE_CSV) as queries_file_obj:
         read_queries(queries_file_obj)
